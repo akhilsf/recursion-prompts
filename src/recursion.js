3258b9dceece82b3cccfe1ce89f68e03baf375ff
Akhil Patel <akhilsf@gmail.com>
2021-05-21 13:58:30 -0700

    Problem 6 - integer range - solved

--- src/recursion.js
+++ src/recursion.js
@@ -78,6 +78,21 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  var array = [];
+
+  if (x < y) {
+    x += 1;
+  } else if (x > y) {
+    x -= 1;
+  }
+
+  if (x === y) {
+    return array;
+  } else {
+    array.push(x);
+  }
+
+  return array.concat(range(x, y));
 };
 
 // 7. Compute the exponent of a number.
