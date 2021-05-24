76b5aa71886556d6c55f07b9b50578363c9a509c
Akhil Patel <akhilsf@gmail.com>
2021-05-24 14:34:31 -0700

    Problem 15 - compareStr - solved

--- src/recursion.js
+++ src/recursion.js
@@ -261,6 +261,13 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1[0] !== str2[0]) {
+    return false;
+  } else if (str1.length === 0 && str2.length === 0) {
+    return true;
+  } else {
+    return compareStr(str1.slice(1, str1.length), str2.slice(1, str2.length));
+  }
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
