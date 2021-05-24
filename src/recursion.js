d8ea8c4b29f8eb406df801a57ce32f63fdb7a58b
Akhil Patel <akhilsf@gmail.com>
2021-05-24 13:48:14 -0700

    Problem 13 - divide - solved

--- src/recursion.js
+++ src/recursion.js
@@ -208,6 +208,30 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return 0;
+  }
+  if (x - y === 0) {
+    return 1;
+  }
+
+  var count = 0;
+
+  if (x < 0 && y > 0) {
+    return -divide(-x, y);
+  } else if (x > 0 && y < 0) {
+    return -divide(x, -y);
+  } else if (x < 0 && y < 0) {
+    return divide (-x, -y);
+  } else if (x > y) {
+    count++;
+    return count + divide(x - y, y);
+  } else {
+    return count;
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
